aef223e783b3c6898368fad4da57cb0812f806d5
Talha Tahir <mr.talhahtahir@gmail.com>
2025-06-28 15:52:39 +0500

    UserServices done!

--- src/main/java/com/talha/academix/services/UserService.java
+++ src/main/java/com/talha/academix/services/UserService.java
@@ -1,5 +1,14 @@
 package com.talha.academix.services;
 
+import java.util.List;
+
+import com.talha.academix.dto.CreateUserDTO;
+import com.talha.academix.dto.UserDTO;
+
 public interface UserService {
-    
+    UserDTO createUser(CreateUserDTO dto);
+    UserDTO getUserById(Long id);
+    List<UserDTO> getAllUsers();
+    void deleteUser(Long id);
+    UserDTO updateUser(Long id, CreateUserDTO dto);
 }
--- src/main/java/com/talha/academix/services/impl/UserServiceImpl.java
+++ src/main/java/com/talha/academix/services/impl/UserServiceImpl.java
@@ -1,5 +1,73 @@
 package com.talha.academix.services.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.talha.academix.dto.CreateUserDTO;
+import com.talha.academix.dto.UserDTO;
+import com.talha.academix.exception.ResourceNotFoundException;
+import com.talha.academix.model.User;
+import com.talha.academix.repository.UserRepo;
+import com.talha.academix.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final ModelMapper modelMapper;
+    @Override
+    public UserDTO createUser(CreateUserDTO dto) {
+        
+        User user = modelMapper.map(dto, User.class);
+        User savedUser = userRepo.save(user);
+        UserDTO userDTO = modelMapper.map(savedUser, UserDTO.class);
+        return userDTO;
+
+        
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+        User user = userRepo.findById(id)
+            .orElseThrow(()-> new ResourceNotFoundException("User not found with id : "+id));
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepo.findAll();
+        return users.stream()
+            .map(user -> modelMapper.map(user, UserDTO.class))
+            .toList();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = userRepo.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("User not found with id : "+id));
+        userRepo.delete(user);
+    }
+
+    @Override
+    public UserDTO updateUser(Long id, CreateUserDTO dto) {
+        User user = userRepo.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found with id : " + id));
+
+            user.setUsername(dto.getUsername());
+            user.setGender(dto.getGender());
+            user.setPassword(dto.getPassword());
+            user.setEmail(dto.getEmail());  
+            user.setRole(dto.getRole());
+            user.setPhone(dto.getPhone());
+            user.setImage(dto.getImage());
+        
+            User updatedUser = userRepo.save(user);
+            return modelMapper.map(updatedUser, UserDTO.class);
+    }
     
 }
