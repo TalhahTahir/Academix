1c3648b10e5f056e25f2c1e2293863f2b9e6eb4d
Talha Tahir <mr.talhahtahir@gmail.com>
2025-06-28 17:16:09 +0500

    Usercontroller done!

--- src/main/java/com/talha/academix/controllers/UserController.java
+++ src/main/java/com/talha/academix/controllers/UserController.java
@@ -1,5 +1,54 @@
 package com.talha.academix.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talha.academix.dto.CreateUserDTO;
+import com.talha.academix.dto.UserDTO;
+import com.talha.academix.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
     
+    private final UserService userService;
+    
+    @PostMapping
+    public UserDTO createUser(@RequestBody CreateUserDTO dto) {
+        return userService.createUser(dto);
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserByID(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<UserDTO> getAllUsers(){
+        return userService.getAllUsers();
+    }
+    
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Long id, @RequestBody CreateUserDTO dto){
+        return userService.updateUser(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        }
 }
